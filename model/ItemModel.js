99e393e8da6456d395f0a5d6afc69be5079a22b4
Uriel Cabrera <59104218+uricabrera@users.noreply.github.com>
2024-11-07 20:53:36 -0300

    Update ItemModel.js

--- model/ItemModel.js
+++ model/ItemModel.js
@@ -3,10 +3,11 @@ const moment = require('moment-timezone');
 
 // Configuración de la conexión a la base de datos
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'item_bd'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASENAME,
+    port: process.env.DB_PORT
 });
 
 // Conectar a la base de datos

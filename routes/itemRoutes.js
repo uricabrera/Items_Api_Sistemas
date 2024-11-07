b864b9a7ca333656bc54f5b0c9d173bfddb51127
Uriel Cabrera <59104218+uricabrera@users.noreply.github.com>
2024-11-07 20:54:42 -0300

    Update itemRoutes.js

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -6,7 +6,7 @@ const itemController = require('../controllers/itemController'); // Importa el c
 router.get('/api/v1', itemController.getAllItems);
 router.get('/api/v1/:id', itemController.getItemById);
 router.post('/api/v1', itemController.createItem);
-router.put('api/v1/:id', itemController.updateItem);
+router.put('/api/v1/:id', itemController.updateItem);
 router.delete('/api/v1/:id', itemController.deleteItem);
 
 module.exports = router;
